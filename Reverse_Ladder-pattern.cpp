63948621014c73d6c953c5cafba8e7c3f70c736d
Vikas Jayavarapu <vikasjayavarapu12@gmail.com>
2025-07-27 22:39:56 +0530

    Create Reverse_Ladder-pattern.cpp

---
+++ Reverse_Ladder-pattern.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    int height = 6;
+
+    for (int i = height; i >= 1; --i) { 
+        
+        for (int j = 1; j <= i; ++j) {  
+            std::cout << " *";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
+
+/*
+Output:
+ * * * * * *
+ * * * * *
+ * * * *
+ * * *
+ * *
+ *
+*/

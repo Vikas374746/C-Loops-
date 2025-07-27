24ae77c079a8e22d250aba1990e8486eab810779
Vikas Jayavarapu <vikasjayavarapu12@gmail.com>
2025-07-27 22:18:14 +0530

    Create Char multiplication(SIT).cpp

---
+++ Char multiplication(SIT).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        std::cout << "SIT" << std::endl;
+    }
+    return 0;
+}
+
+/*
+Output:
+SIT
+SIT
+SIT
+SIT
+SIT
+*/
